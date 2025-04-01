fbb89d9a98b7d121108fef79140741b6f8117a6b
Praveena Pradeep <168461511+Praveena-Pradeep@users.noreply.github.com>
2025-04-01 06:54:45 +0530

    Commit changes to hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-import sys
-
-def main():
-    print('Hello there,', sys.argv[0])
-if __name__ == "__main__":
-     main()
+def factorial(n):
+       if n==0:
+         return 1
+       else:
+         return n*factorial(n-1)
+         
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}.")
